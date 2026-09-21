30ee3caaa2a926a467ac1c1ce63f52b15f8199e0
Orvlyn <RoadEditor17@gmail.com>
2026-09-22 02:52:30 +1000

    fix: register DINKFLIX through File Transformation

--- src/Jellyfin.Plugin.DinkFlix/DinkFlixServiceRegistrator.cs
+++ src/Jellyfin.Plugin.DinkFlix/DinkFlixServiceRegistrator.cs
@@ -1,14 +1,16 @@
 using MediaBrowser.Controller;
 using MediaBrowser.Controller.Plugins;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Jellyfin.Plugin.DinkFlix;
 
+/// <summary>
+/// Registers DINKFLIX with Jellyfin's File Transformation plugin.
+/// </summary>
 public sealed class DinkFlixServiceRegistrator : IPluginServiceRegistrator
 {
     public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
     {
-        serviceCollection.AddTransient<IStartupFilter, DinkFlixStartupFilter>();
+        serviceCollection.AddHostedService<FileTransformationRegistrationService>();
     }
 }
