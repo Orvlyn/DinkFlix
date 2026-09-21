87f66c5e6dfc8d06201749c252289005d9a7763a
Orvlyn <RoadEditor17@gmail.com>
2026-09-22 07:27:06 +1000

    Delete src/Jellyfin.Plugin.SleekFin directory

--- src/Jellyfin.Plugin.SleekFin/Model/PatchRequestPayload.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace Jellyfin.Plugin.SleekFin.Model;
-
-public sealed class PatchRequestPayload
-{
-    [JsonPropertyName("contents")]
-    public string? Contents { get; set; }
-}
