914a99251f16cd8f6d134467ce0d491e12915526
Orvlyn <RoadEditor17@gmail.com>
2026-09-24 21:16:50 +1000

    fix: Stop detail action observer loop

--- src/Jellyfin.Plugin.SleekFin/Frontend/features/details/actions.js
+++ src/Jellyfin.Plugin.SleekFin/Frontend/features/details/actions.js
@@ -28,7 +28,7 @@ export function createActions(container, isEpisode) {
   observer.observe(container, {
     attributes: true,
     subtree: true,
-    attributeFilter: ['data-isfavorite', 'data-action', 'class'],
+    attributeFilter: ['data-isfavorite', 'data-action'],
   });
 
   return {
